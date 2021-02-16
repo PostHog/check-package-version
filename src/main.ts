ddb3297fec1eabbc99f9a3799bd8b471670dd6f5
Michael Matloka <dev@twixes.com>
2021-02-16 13:08:49 +0100

    Write action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,40 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import packageJson from 'package-json'
+import * as path from 'path'
+import * as fs from 'fs'
+
+/** An incomplete representation of package.json. */
+interface PackageFile {
+    name: string
+    version: string
+    [key: string]: any
+}
+
+async function readPackageFile(packagePath: string): Promise<PackageFile> {
+    return await new Promise((resolve, reject) => {
+        const packageFilePath = path.join(packagePath, 'package.json')
+        core.debug(`Reading ${packageFilePath}…`)
+        try {
+            fs.readFile(packageFilePath, (err, data) => {
+                if (err) reject(err)
+                resolve(JSON.parse(data.toString()))
+            })
+        } catch (err) {
+            reject(err)
+        }
+    }
+}
 
 async function run(): Promise<void> {
     try {
-        const ms: string = core.getInput('milliseconds')
-        core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-        core.debug(new Date().toTimeString())
-        await wait(parseInt(ms, 10))
-        core.debug(new Date().toTimeString())
-
-        core.setOutput('time', new Date().toTimeString())
+        const packagePath = core.getInput('path') || '.'
+        const packageFile = await readPackageFile(packagePath)
+        core.debug(`Fetching package ${packageFile.name} information from npm…`)
+        const packageNpm = await packageJson(packageFile.name, {allVersions: true})
+        const isUnpublishedVersion = !Object.keys(packageNpm.versions).includes(packageFile.version)
+        core.setOutput('is-unpublished-version', isUnpublishedVersion.toString())
+        core.setOutput('latest-published-version', packageNpm['dist-tags'].latest)
+        core.setOutput('repo-version', packageFile.version)
     } catch (error) {
         core.setFailed(error.message)
     }
